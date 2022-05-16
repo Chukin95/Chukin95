3625e6d19d5bb9f4a6ff5138167988fa69224eb9
Cristian Gómez <54732592+Chukin95@users.noreply.github.com>
2022-05-16 02:48:50 -0300

    1232

--- js/tateti.js
+++ js/tateti.js
@@ -1,5 +1,5 @@
 let turnoPlayer = 1;
-nuevo()
+nuevo(false)
 
 function presiona(id) {
     if (!botonOcupado(id)) {
@@ -33,31 +33,38 @@ function presiona(id) {
     }
 }
 
-function nuevo() {
+function nuevo(conMensaje = true) {
     turnoPlayer = 1;
     botonNuevo = document.getElementById("new");
     botonNuevo.setAttribute('style', 'background-color: grey');
     display = document.getElementById("display-tateti")
     display.innerHTML = "Turno del jugador 1";
     display.setAttribute('style', 'background-color: red');
-    document.getElementById("11").innerHTML = "-";
-    document.getElementById("11").setAttribute('style', 'background-color: gray');
-    document.getElementById("12").innerHTML = "-";
-    document.getElementById("12").setAttribute('style', 'background-color: gray');
-    document.getElementById("13").innerHTML = "-";
-    document.getElementById("13").setAttribute('style', 'background-color: gray');
-    document.getElementById("21").innerHTML = "-";
-    document.getElementById("21").setAttribute('style', 'background-color: gray');
-    document.getElementById("22").innerHTML = "-";
-    document.getElementById("22").setAttribute('style', 'background-color: gray');
-    document.getElementById("23").innerHTML = "-";
-    document.getElementById("23").setAttribute('style', 'background-color: gray');
-    document.getElementById("31").innerHTML = "-";
-    document.getElementById("31").setAttribute('style', 'background-color: gray');
-    document.getElementById("32").innerHTML = "-";
-    document.getElementById("32").setAttribute('style', 'background-color: gray');
-    document.getElementById("33").innerHTML = "-";
-    document.getElementById("33").setAttribute('style', 'background-color: gray');
+    document.getElementById("1").innerHTML = "-";
+    document.getElementById("1").setAttribute('style', 'background-color: gray');
+    document.getElementById("2").innerHTML = "-";
+    document.getElementById("2").setAttribute('style', 'background-color: gray');
+    document.getElementById("3").innerHTML = "-";
+    document.getElementById("3").setAttribute('style', 'background-color: gray');
+    document.getElementById("4").innerHTML = "-";
+    document.getElementById("4").setAttribute('style', 'background-color: gray');
+    document.getElementById("5").innerHTML = "-";
+    document.getElementById("5").setAttribute('style', 'background-color: gray');
+    document.getElementById("6").innerHTML = "-";
+    document.getElementById("6").setAttribute('style', 'background-color: gray');
+    document.getElementById("7").innerHTML = "-";
+    document.getElementById("7").setAttribute('style', 'background-color: gray');
+    document.getElementById("8").innerHTML = "-";
+    document.getElementById("8").setAttribute('style', 'background-color: gray');
+    document.getElementById("9").innerHTML = "-";
+    document.getElementById("9").setAttribute('style', 'background-color: gray');
+    if (conMensaje) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Nueva partida',
+            text: 'Has empezado una nueva partida!'
+        });
+    }
 }
 
 function botonOcupado(id) {
@@ -72,7 +79,7 @@ function cambioJugador() {
     display = document.getElementById("display-tateti");
     if (turnoPlayer == 1) {
         turnoPlayer = 2;
-        display.innerHTML = "Turno del jugador 2";
+        display.innerHTML = "Turno del jugador 2";9
         display.setAttribute('style', 'background-color: blue');
     } else if (turnoPlayer == 2) {
         turnoPlayer = 1;
@@ -82,37 +89,37 @@ function cambioJugador() {
 }
 function buscarGanador(conGanador) {
     display = document.getElementById("display-tateti");
-    boton11 = document.getElementById("11");
-    boton12 = document.getElementById("12");
-    boton13 = document.getElementById("13");
-    boton21 = document.getElementById("21");
-    boton22 = document.getElementById("22");
-    boton23 = document.getElementById("23");
-    boton31 = document.getElementById("31");
-    boton32 = document.getElementById("32");
-    boton33 = document.getElementById("33");
-    if (boton11.textContent == "X" && boton12.textContent == "X" && boton13.textContent == "X"
-        || boton21.textContent == "X" && boton22.textContent == "X" && boton23.textContent == "X"
-        || boton31.textContent == "X" && boton32.textContent == "X" && boton33.textContent == "X"
-        || boton11.textContent == "X" && boton21.textContent == "X" && boton31.textContent == "X"
-        || boton12.textContent == "X" && boton22.textContent == "X" && boton32.textContent == "X"
-        || boton13.textContent == "X" && boton23.textContent == "X" && boton33.textContent == "X"
-        || boton11.textContent == "X" && boton22.textContent == "X" && boton33.textContent == "X"
-        || boton13.textContent == "X" && boton22.textContent == "X" && boton31.textContent == "X") {
+    boton1 = document.getElementById("1");
+    boton2 = document.getElementById("2");
+    boton3 = document.getElementById("3");
+    boton4 = document.getElementById("4");
+    boton5 = document.getElementById("5");
+    boton6 = document.getElementById("6");
+    boton7 = document.getElementById("7");
+    boton8 = document.getElementById("8");
+    boton9 = document.getElementById("9");
+    if (boton1.textContent == "X" && boton2.textContent == "X" && boton3.textContent == "X"
+        || boton4.textContent == "X" && boton5.textContent == "X" && boton6.textContent == "X"
+        || boton7.textContent == "X" && boton8.textContent == "X" && boton9.textContent == "X"
+        || boton1.textContent == "X" && boton4.textContent == "X" && boton7.textContent == "X"
+        || boton2.textContent == "X" && boton5.textContent == "X" && boton8.textContent == "X"
+        || boton3.textContent == "X" && boton6.textContent == "X" && boton9.textContent == "X"
+        || boton1.textContent == "X" && boton5.textContent == "X" && boton9.textContent == "X"
+        || boton3.textContent == "X" && boton5.textContent == "X" && boton7.textContent == "X") {
         if (conGanador) {
             Swal.fire('Ganó el jugador 1!');
         }
         display.innerHTML = "¡Ganó el jugador 1!";
         display.setAttribute('style', 'background-color: red');
         return true;
-    } else if (boton11.textContent == "O" && boton12.textContent == "O" && boton13.textContent == "O"
-        || boton21.textContent == "O" && boton22.textContent == "O" && boton23.textContent == "O"
-        || boton31.textContent == "O" && boton32.textContent == "O" && boton33.textContent == "O"
-        || boton11.textContent == "O" && boton21.textContent == "O" && boton31.textContent == "O"
-        || boton12.textContent == "O" && boton22.textContent == "O" && boton32.textContent == "O"
-        || boton13.textContent == "O" && boton23.textContent == "O" && boton33.textContent == "O"
-        || boton11.textContent == "O" && boton22.textContent == "O" && boton33.textContent == "O"
-        || boton13.textContent == "O" && boton22.textContent == "O" && boton31.textContent == "O") {
+    } else if (boton1.textContent == "O" && boton2.textContent == "O" && boton3.textContent == "O"
+        || boton4.textContent == "O" && boton5.textContent == "O" && boton6.textContent == "O"
+        || boton7.textContent == "O" && boton8.textContent == "O" && boton9.textContent == "O"
+        || boton1.textContent == "O" && boton4.textContent == "O" && boton7.textContent == "O"
+        || boton2.textContent == "O" && boton5.textContent == "O" && boton8.textContent == "O"
+        || boton3.textContent == "O" && boton6.textContent == "O" && boton9.textContent == "O"
+        || boton1.textContent == "O" && boton5.textContent == "O" && boton9.textContent == "O"
+        || boton3.textContent == "O" && boton5.textContent == "O" && boton7.textContent == "O") {
         if (conGanador) {
             Swal.fire('Ganó el jugador 2');
         }
