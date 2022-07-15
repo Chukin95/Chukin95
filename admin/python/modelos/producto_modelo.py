9140aca8005cc528d5439e08a98b9536512caeef
Cristian Gomez <svpr0@hotmail.com>
2022-07-14 21:22:51 -0300

    Update producto_modelo.py

--- admin/python/modelos/producto_modelo.py
+++ admin/python/modelos/producto_modelo.py
@@ -5,7 +5,7 @@ class Producto(db.Model):  # defino la tabla
     nombre=db.Column(db.String(100))
     descripcion=db.Column(db.String(500))
     categoria=db.Column(db.Integer)
-    precio=db.Column(db.Float(15))
+    precio=db.Column(db.Float)
     stock=db.Column(db.Integer)
     imagen=db.Column(db.String(255))
     descuento=db.Column(db.Integer)
