d292401da5f4a1a3ce0ba455b61cf8518c3a2b6c
Cristian Gomez <svpr0@hotmail.com>
2022-07-17 23:42:43 -0300

    filtros

--- admin/app.py
+++ admin/app.py
@@ -1,25 +1,20 @@
-from flask import Flask ,jsonify,request
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
-app=Flask(__name__)   #creamos el objeto app
-CORS(app)     # soluciona el error cuando el frontend accede a los endpoint que genera el backend
+app = Flask(__name__)  # creamos el objeto app
+# soluciona el error cuando el frontend accede a los endpoint que genera el backend
+CORS(app)
 
-# app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/flaskmysql'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql10505964:fwbFfGGbbZ@sql10.freemysqlhosting.net/sql10505964'  # Configuro la propiedad SQLALCHEMY_DATABASE_URI
-#                                          usuario:clave@localhost/nombreBaseDecategorias
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-db= SQLAlchemy(app)
-ma=Marshmallow(app)
-
-from python.controladores.producto_controlador import *
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql10505964:fwbFfGGbbZ@sql10.freemysqlhosting.net/sql10505964'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 from python.controladores.categoria_controlador import *
-
+from python.controladores.producto_controlador import *
 
 # programa principal
-if __name__=='__main__': 
-       app.run(debug=True, port=5000) 
-       
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
--- admin/python/controladores/producto_controlador.py
+++ admin/python/controladores/producto_controlador.py
@@ -1,63 +1,132 @@
-#controlador es el intermediario entre la vista y el modelo
+# controlador es el intermediario entre la vista y el modelo
 # aqui ocurre la logica del programa
 from app import *
 from python.modelos.producto_modelo import *
+from sqlalchemy import desc, asc
 
-@app.route('/productos',methods=['GET'])
+
+@app.route('/productos', methods=['GET'])
 def get_Productos():
-    all_productos=Producto.query.all()   # es una lista
-    result=productos_schema.dump(all_productos)
+    productos_filtrados = Producto.query.all()   # es una lista
+    result = productos_schema.dump(productos_filtrados)
     return jsonify(result)
- 
-@app.route('/productos/<id>',methods=['GET'])
+
+
+@app.route('/productos/<id>', methods=['GET'])
 def get_producto(id):
-    producto= Producto.query.get(id)
+    producto = Producto.query.get(id)
     return producto_schema.jsonify(producto)
 
-@app.route('/productos', methods=['POST']) # crea ruta o endpoint
-def create_producto():
-    #print(request.json)  # request.json contiene el json que envio el cliente
-    nombre=request.json['nombre']
-    descripcion=request.json['descripcion']
-    categoria=request.json['categoria']
-    precio=request.json['precio']
-    stock=request.json['stock']
-    imagen=request.json['imagen']
-    descuento=request.json['descuento']
 
-    new_producto=Producto(nombre,descripcion,categoria, precio,stock,imagen,descuento)
+@app.route('/productos', methods=['POST'])  # crea ruta o endpoint
+def create_producto():
+    # print(request.json)  # request.json contiene el json que envio el cliente
+    nombre = request.json['nombre']
+    descripcion = request.json['descripcion']
+    categoria = request.json['categoria']
+    precio = request.json['precio']
+    stock = request.json['stock']
+    imagen = request.json['imagen']
+    descuento = request.json['descuento']
+
+    new_producto = Producto(nombre, descripcion, categoria,
+                            precio, stock, imagen, descuento)
     db.session.add(new_producto)
     db.session.commit()
     return producto_schema.jsonify(new_producto)
 
 
-@app.route('/productos/<id>' ,methods=['PUT'])
+@app.route('/productos/<id>', methods=['PUT'])
 def update_producto(id):
-    producto=Producto.query.get(id)   
-   
-    nombre=request.json['nombre']
-    descripcion=request.json['descripcion']
-    categoria=request.json['categoria']
-    precio=request.json['precio']
-    stock=request.json['stock']
-    imagen=request.json['imagen']
-    descuento=request.json['descuento']
- 
-    producto.nombre=nombre
-    producto.descripcion=descripcion
-    producto.categoria=categoria
-    producto.precio=precio
-    producto.stock=stock
-    producto.imagen=imagen
-    producto.descuento=descuento
-    
+    producto = Producto.query.get(id)
+
+    nombre = request.json['nombre']
+    descripcion = request.json['descripcion']
+    categoria = request.json['categoria']
+    precio = request.json['precio']
+    stock = request.json['stock']
+    imagen = request.json['imagen']
+    descuento = request.json['descuento']
+
+    producto.nombre = nombre
+    producto.descripcion = descripcion
+    producto.categoria = categoria
+    producto.precio = precio
+    producto.stock = stock
+    producto.imagen = imagen
+    producto.descuento = descuento
+
     db.session.commit()
     return producto_schema.jsonify(producto)
- 
 
-@app.route('/productos/<id>',methods=['DELETE'])
+
+@app.route('/productos/<id>', methods=['DELETE'])
 def delete_producto(id):
-    producto=Producto.query.get(id)
+    producto = Producto.query.get(id)
     db.session.delete(producto)
     db.session.commit()
     return producto_schema.jsonify(producto)
+
+
+@app.route('/productos/filtros', methods=['GET'])
+def get_ProductosFiltrados(): # filtrar por categoria, nombre, precio, stock, descuento ordenar por precio nombre
+    categoria = request.args.get('categoria')
+    filtrar = request.args.get('filtrar')
+    orden = request.args.get('orden')
+    if categoria == "":
+        categoria = None
+    if filtrar == "":
+        filtrar = None
+    if orden == "":
+        orden = 'DESC'
+    
+    if categoria is not None:
+        if filtrar == 'precio':
+            if orden == 'ASC':
+                productos_filtrados = Producto.query.filter_by(categoria=categoria).order_by(asc(Producto.precio)).all()
+            else:
+                productos_filtrados = Producto.query.filter_by(categoria=categoria).order_by(desc(Producto.precio)).all()
+        elif filtrar == 'nombre':
+            if orden == 'ASC':
+                productos_filtrados = Producto.query.filter_by(categoria=categoria).order_by(asc(Producto.nombre)).all()
+            else:
+                productos_filtrados = Producto.query.filter_by(categoria=categoria).order_by(desc(Producto.nombre)).all()
+        elif filtrar == 'stock':
+            if orden == 'ASC':
+                productos_filtrados = Producto.query.filter_by(categoria=categoria).order_by(asc(Producto.stock)).all()
+            else:
+                productos_filtrados = Producto.query.filter_by(categoria=categoria).order_by(desc(Producto.stock)).all()
+        elif filtrar == 'descuento':
+            if orden == 'ASC':
+                productos_filtrados = Producto.query.filter_by(categoria=categoria).order_by(asc(Producto.descuento)).all()
+            else:
+                productos_filtrados = Producto.query.filter_by(categoria=categoria).order_by(desc(Producto.descuento)).all()
+    elif filtrar is not None:
+        if filtrar == 'precio':
+            if orden == 'ASC':
+                productos_filtrados = Producto.query.order_by(asc(Producto.precio)).all()
+            else:
+                productos_filtrados = Producto.query.order_by(desc(Producto.precio)).all()
+        elif filtrar == 'nombre':
+            if orden == 'ASC':
+                productos_filtrados = Producto.query.order_by(asc(Producto.nombre)).all()
+            else:
+                productos_filtrados = Producto.query.order_by(desc(Producto.nombre)).all()
+        elif filtrar == 'stock':
+            if orden == 'ASC':
+                productos_filtrados = Producto.query.order_by(asc(Producto.stock)).all()
+            else:
+                productos_filtrados = Producto.query.order_by(desc(Producto.stock)).all()
+        elif filtrar == 'descuento':
+            if orden == 'ASC':
+                productos_filtrados = Producto.query.order_by(asc(Producto.descuento)).all()
+            else:
+                productos_filtrados = Producto.query.order_by(desc(Producto.descuento)).all()
+    else:
+        productos_filtrados = Producto.query.all()
+    result = productos_schema.dump(productos_filtrados)
+    #productos_filtrados = Producto.query.filter(categoria=request.args.get('categoria'), filtrar=request.args.get('filtrar')).order_by(request.args.get('orden'))
+    #productos_filtrados = Producto.query.filter(categoria=request.args.get('categoria')).order_by(request.args.get('orden'))
+
+    #result = productos_schema.dump(productos_filtrados)
+    return jsonify(result)
